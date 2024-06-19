146b16487719ff4b3828ab6df8e76f783e2c9c30
EkaGontsova <156356137+EkaGontsova@users.noreply.github.com>
2024-06-19 16:52:49 +0300

    Update database.py

--- database.py
+++ database.py
@@ -1,11 +1,11 @@
 import os
 import sqlalchemy as sq
 from dotenv import load_dotenv
-from sqlalchemy import create_engine, text
+from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker, relationship
 
-Base = declarative_base()
 load_dotenv()
+Base = declarative_base()
 
 
 class User(Base):
@@ -15,6 +15,7 @@ class User(Base):
     gender = sq.Column(sq.String)
     city = sq.Column(sq.String)
     photos = relationship('Photo', back_populates='user')
+    favorites = relationship('Favorite', foreign_keys='[Favorite.user_id]', back_populates='user')
 
 
 class Photo(Base):
@@ -31,6 +32,8 @@ class Favorite(Base):
     id = sq.Column(sq.Integer, primary_key=True)
     user_id = sq.Column(sq.Integer, sq.ForeignKey('users.id'))
     favorite_user_id = sq.Column(sq.Integer, sq.ForeignKey('users.id'))
+    user = relationship('User', foreign_keys=[user_id], back_populates='favorites')
+    favorite_user = relationship('User', foreign_keys=[favorite_user_id], backref='favorited_by')
 
 
 class Blacklist(Base):
@@ -47,31 +50,45 @@ def create_session():
     return Session()
 
 
-# Создание таблиц в базе данных
 def create_tables(engine):
     Base.metadata.drop_all(engine)
     Base.metadata.create_all(engine)
 
 
-def add_to_favorites(session, user_id, favorite_user_id):
-    """Добавление пользователя в избранное."""
+def add_user(session, age, gender, city):
+    new_user = User(age=age, gender=gender, city=city)
+    session.add(new_user)
+    try:
+        session.commit()
+        return new_user.id
+    except Exception as e:
+        session.rollback()
+        print(f'Ошибка при добавлении пользователя: {e}')
+        raise e
+
+
+def add_favorite_user(session, user_id, favorite_user_id):
     favorite = Favorite(user_id=user_id, favorite_user_id=favorite_user_id)
     session.add(favorite)
-    session.commit()
 
-
-def add_to_blacklist(session, user_id, blacklisted_user_id):
-    """Добавление пользователя в черный список."""
-    blacklist = Blacklist(user_id=user_id, blacklisted_user_id=blacklisted_user_id)
-    session.add(blacklist)
-    session.commit()
+    try:
+        session.commit()  
+    except Exception as e:
+        session.rollback()  
+        print(f'Ошибка при добавлении пользователя в избранное: {e}')
+        raise e
 
 
 def add_photos(session, user_id, photo_urls):
-    """Добавление фотографии пользователя."""
     for url in photo_urls:
         photo = Photo(user_id=user_id, url=url)
         session.add(photo)
+    session.commit()
+    
+    
+def add_to_blacklist(session, user_id, blacklisted_user_id):
+    blacklist = Blacklist(user_id=user_id, blacklisted_user_id=blacklisted_user_id)
+    session.add(blacklist)
     session.commit()
 
 
