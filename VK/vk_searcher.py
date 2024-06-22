d76429af8784c12fe54f9b69bf158769eb3ce221
EkaGontsova <156356137+EkaGontsova@users.noreply.github.com>
2024-06-22 12:50:59 +0300

    Add docstrings

--- VK/vk_searcher.py
+++ VK/vk_searcher.py
@@ -4,6 +4,7 @@ import requests
 
 
 class VK:
+    """ Класс для работы с VK API"""
     BASE_URL = 'https://api.vk.com/method/'
     BASE_VK_URL = 'https://vk.com/'
 
@@ -14,7 +15,14 @@ class VK:
         self.params = {'access_token': self.token, 'v': self.version}
 
     def get_city_id(self, city):
-
+        """Получение id города
+        Args:
+            city (str): Название города, для которого необходимо получить id.
+        Returns:
+            int: Идентификатор города в базе данных VK.
+        Raises:
+            ValueError: Если не удалось найти город.
+        """
         params = {'q': city}
         params.update(self.params)
         response = requests.get(f'{self.BASE_URL}database.getCities', params=params)
@@ -27,6 +35,14 @@ class VK:
         return find_city_id
 
     def get_photos(self, owner_id, album_id='profile', count=3):
+        """Получение фотографий пользователя
+        Args:
+            owner_id (int): Идентификатор пользователя VK, фотографии которого нужно получить.
+            album_id (str): Идентификатор альбома, из которого нужно получить фотографии. По умолчанию 'profile'.
+            count (int): Количество фотографий, которое нужно вернуть. По умолчанию 3.
+        Returns:
+            list: Список словарей с информацией о фотографиях пользователя
+        """
         params = {'owner_id': owner_id, 'album_id': album_id, 'extended': 0, 'count': count}
         params.update(self.params)
         response = requests.get(f'{self.BASE_URL}photos.get', params=params)
@@ -34,6 +50,13 @@ class VK:
         return response.json()['response']['items']
 
     def get_user_params(self, user_with_params):
+        """" Получение необходимых параметров пользователя
+        Args:
+            user_with_params (dict): Словарь с информацией о пользователе.
+
+        Returns:
+            dict: Словарь с информацией о пользователе.
+        """
         find_user = {}
         photo_list = []
         find_user_photos = self.get_photos(user_with_params['id'])
@@ -42,13 +65,20 @@ class VK:
             size_params = current_photo['sizes'][-1]['url']
             photo_list.append(size_params)
 
-        find_user['name_and_last_name'] = f'{user_with_params['first_name']} {user_with_params['last_name']}'
-        find_user['urs'] = f'{self.BASE_VK_URL}id{user_with_params['id']}'
+        find_user['name_and_last_name'] = f"{user_with_params['first_name']} {user_with_params['last_name']}"
+        find_user['urs'] = f"{self.BASE_VK_URL}id{user_with_params['id']}"
         find_user['photos'] = photo_list
 
         return find_user
 
     def get_user_info(self, user_id):
+        """Получение информации о пользователе
+        Args:
+            user_id:
+                int: Идентификатор пользователя VK
+        Returns:
+            dict: Словарь с информацией о пользователе
+        """
         params = {'user_ids': user_id}
         params.update(self.params)
         response = requests.get(f'{self.BASE_URL}users.get', params=params)
@@ -56,6 +86,13 @@ class VK:
         return response.json()
 
     def search_user(self, user_params):
+        """Поиск пользователя
+        Args:
+            user_params:
+                dict: Словарь с параметрами поиска
+        Returns:
+            dict: Словарь с информацией о случайно выбранном пользователе
+        """
         city_id = self.get_city_id(user_params['city'])
         params = {'age_from': user_params['age_from'], 'city': city_id, 'sex': user_params['sex']}
         if user_params['age_to'] != 0:
